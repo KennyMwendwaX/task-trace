fd0fc7ac563c224fdbb6b27e6da9c7ccfd719be8
IconicDexter <kennymwendwa67@gmail.com>
2023-09-19 08:00:46 +0300

    Changed font from inter to roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["400"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
       </body>
