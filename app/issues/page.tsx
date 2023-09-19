8a8d3619119bdb697e803c1698df473786c14288
IconicDexter <kennymwendwa67@gmail.com>
2023-09-19 08:29:29 +0300

    Added a select to filter issues by status

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,9 +1,33 @@
 "use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 export default function Issues() {
   return (
     <>
-      <div className="container mx-auto mt-4 px-5 pb-5 pt-12">Issues</div>
+      <div className="container mx-auto mt-4 px-5 pb-5 pt-12">
+        Issues
+        <div>
+          <Select>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Filter by status" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem defaultChecked value="all-issues">
+                All Issues
+              </SelectItem>
+              <SelectItem value="open">Open</SelectItem>
+              <SelectItem value="in-progress">In progress</SelectItem>
+              <SelectItem value="closed">Closed</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+      </div>
     </>
   );
 }
