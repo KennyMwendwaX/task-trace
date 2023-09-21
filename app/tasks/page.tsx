1e06036bd31b4b0b5a0d7d70f78bed4f022d121b
IconicDexter <kennymwendwa67@gmail.com>
2023-09-21 18:29:15 +0300

    Created tasks.json

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -46,7 +46,7 @@ export default async function TaskPage() {
           className="hidden dark:block"
         />
       </div>
-      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+      <div className="container mx-auto mt-4 px-5 pb-5 pt-12">
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
