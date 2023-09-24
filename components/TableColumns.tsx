8a565ddc280024adef458f32b103de6bfb97e1c0
IconicDexter <kennymwendwa67@gmail.com>
2023-09-24 15:49:38 +0300

    Configured the first column the checkbox

--- components/TableColumns.tsx
+++ components/TableColumns.tsx
@@ -8,11 +8,28 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { labels, priorities, statuses } from "@/data/IconMappingOptions";
 import { Task } from "@/data/schema";
 import TableColumnHeader from "./TableColumnHeader";
+import TableRowActions from "./TableRowActions";
 
-export default function TableColumns() {
-  return (
-    <>
-      <div>TableColumns</div>
-    </>
-  );
-}
+export const TableColumns: ColumnDef<Task>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select-all"
+        className="translate-y-[2px]"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select-row"
+        className="translate-y-[2px]"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+];
