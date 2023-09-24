563a46665735aa3fd60f8bc29fb2611366a288f0
IconicDexter <kennymwendwa67@gmail.com>
2023-09-24 15:15:30 +0300

    Created TableColumns.tsx

---
+++ components/TableColumns.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { Badge } from "@/components/ui/badge";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import { labels, priorities, statuses } from "@/data/IconMappingOptions";
+import { Task } from "@/data/schema";
+import TableColumnHeader from "./TableColumnHeader";
+
+export default function TableColumns() {
+  return (
+    <>
+      <div>TableColumns</div>
+    </>
+  );
+}
