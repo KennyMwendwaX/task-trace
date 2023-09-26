e52b0cfaa80b01bd9e92aeb844a74c2d1847eccb
IconicDexter <kennymwendwa67@gmail.com>
2023-09-26 11:17:53 +0300

    Added TableBody on TaskTable component

--- components/TaskTable.tsx
+++ components/TaskTable.tsx
@@ -84,6 +84,32 @@ export default function TaskTable<TData, TValue>({
                 </TableRow>
               ))}
             </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}>
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center">
+                    No results
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
           </Table>
         </div>
       </div>
