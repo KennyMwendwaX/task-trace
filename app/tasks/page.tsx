13486ee001f21a1cd3bc91c663035c44f78cebc2
IconicDexter <kennymwendwa67@gmail.com>
2023-09-26 11:59:09 +0300

    Added the TaskTable to Page component and passed data and columns props

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,12 +1,22 @@
 "use client";
-import IssuesTable from "@/components/IssuesTable";
+
+import { TableColumns } from "@/components/TableColumns";
+import TaskTable from "@/components/TaskTable";
+import { tasks } from "@/data/tasks";
 
 export default function Issues() {
   return (
     <>
       <div className="container mx-auto mt-4 px-5 pb-5 pt-12">
-        Issues
-        <IssuesTable />
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+        </div>
+        <TaskTable data={tasks} columns={TableColumns} />
       </div>
     </>
   );
