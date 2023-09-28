fa6f81eb78792af516cad88fe578df1460c1e687
IconicDexter <kennymwendwa67@gmail.com>
2023-09-28 06:52:42 +0300

    Changed placeholder on search tasks input

--- components/TableToolbar.tsx
+++ components/TableToolbar.tsx
@@ -21,7 +21,7 @@ export default function TableToolbar<TData>({
       <div className="flex items-center justify-between">
         <div className="flex flex-1 items-center space-x-2">
           <Input
-            placeholder="Filter tasks..."
+            placeholder="Search tasks..."
             value={(table.getColumn("title")?.getFilterValue() as string) ?? ""}
             onChange={(event) =>
               table.getColumn("title")?.setFilterValue(event.target.value)
