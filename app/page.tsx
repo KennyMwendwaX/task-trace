0939bac4a9b6b1fcedb6de8c1ec2a6e6fa2c4a24
IconicDexter <kennymwendwa67@gmail.com>
2023-09-30 13:58:44 +0300

    Created TaskTracker.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import TaskChart from "@/components/TaskChart";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto mt-4 px-5 pb-5 pt-12">Home</div>
+      <div className="container mx-auto mt-4 px-5 pb-5 pt-12">
+        <TaskChart />
+      </div>
     </>
   );
 }
