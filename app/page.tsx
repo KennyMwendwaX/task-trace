6df3dc7d969949fec2717a6b135afc7839767624
IconicDexter <kennymwendwa67@gmail.com>
2023-10-01 14:34:43 +0300

    Added task data to TaskOverview.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Overview from "@/components/Overview";
+import TaskOverview from "@/components/TaskOverview";
 import TaskChart from "@/components/TaskChart";
 
 export default function Home() {
   return (
     <>
       <div className="container mx-auto mt-4 px-5 pb-5 pt-12">
-        <Overview />
+        <TaskOverview />
         <TaskChart />
       </div>
     </>
--- components/TaskOverview.tsx
+++ components/TaskOverview.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { tasks } from "@/data/tasks";
 import {
   CheckCircledIcon,
   CircleIcon,
@@ -8,66 +9,82 @@ import {
 } from "@radix-ui/react-icons";
 
 export default function TaskOverview() {
+  const tasksDone = tasks.filter((task) => task.status === "done");
+  const tasks_done_percentage = (tasksDone.length / tasks.length) * 100;
+
+  const tasksTodo = tasks.filter((task) => task.status === "todo");
+  const tasks_todo_percentage = (tasksTodo.length / tasks.length) * 100;
+
+  const tasksInProgress = tasks.filter((task) => task.status === "in progress");
+  const tasks_inProgress_percentage =
+    (tasksInProgress.length / tasks.length) * 100;
+
+  const tasksCanceled = tasks.filter((task) => task.status === "done");
+  const tasks_canceled_percentage = (tasksCanceled.length / tasks.length) * 100;
+
+  const tasksBacklog = tasks.filter((task) => task.status === "done");
+  const tasks_backlog_percentage = (tasksBacklog.length / tasks.length) * 100;
+
   return (
     <>
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-5">
-        <Card>
+        <Card className="border-slate-500">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Done</CardTitle>
-            <CheckCircledIcon className="h-5 w-5" />
+            <CheckCircledIcon className="h-5 w-5 text-green-600" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">$45,231.89</div>
+            <div className="text-2xl font-bold">{tasksDone.length}</div>
             <p className="text-xs text-muted-foreground">
-              +20.1% from last month
+              {tasks_done_percentage}% of all tasks
             </p>
           </CardContent>
         </Card>
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">In Progess</CardTitle>
-            <StopwatchIcon className="h-5 w-5" />
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">+2350</div>
-            <p className="text-xs text-muted-foreground">
-              +180.1% from last month
-            </p>
-          </CardContent>
-        </Card>
-        <Card>
+        <Card className="border-slate-500">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Todo</CardTitle>
-            <CircleIcon className="h-5 w-5" />
+            <CircleIcon className="h-5 w-5 text-blue-600" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+12,234</div>
+            <div className="text-2xl font-bold">{tasksTodo.length}</div>
             <p className="text-xs text-muted-foreground">
-              +19% from last month
+              {tasks_todo_percentage}% of all tasks
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="border-slate-500">
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-sm font-medium">In Progress</CardTitle>
+            <StopwatchIcon className="h-5 w-5 text-orange-600" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{tasksInProgress.length}</div>
+            <p className="text-xs text-muted-foreground">
+              {tasks_inProgress_percentage}% of all tasks
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="border-slate-500">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Canceled</CardTitle>
-            <CrossCircledIcon className="h-5 w-5" />
+            <CrossCircledIcon className="h-5 w-5 text-red-600" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+573</div>
+            <div className="text-2xl font-bold">{tasksCanceled.length}</div>
             <p className="text-xs text-muted-foreground">
-              +201 since last hour
+              {tasks_canceled_percentage}% of all tasks
             </p>
           </CardContent>
         </Card>
-        <Card>
+        <Card className="border-slate-500">
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Backlog</CardTitle>
-            <QuestionMarkCircledIcon className="h-5 w-5" />
+            <QuestionMarkCircledIcon className="h-5 w-5 text-gray-500" />
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold">+573</div>
+            <div className="text-2xl font-bold">{tasksBacklog.length}</div>
             <p className="text-xs text-muted-foreground">
-              +201 since last hour
+              {tasks_backlog_percentage}% of all tasks
             </p>
           </CardContent>
         </Card>
