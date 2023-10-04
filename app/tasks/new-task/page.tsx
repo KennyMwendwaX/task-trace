a3e0a32627a1e57dd78cd153589151d4ac2b5f48
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-04 17:27:10 +0300

    Changed color on button

--- app/tasks/new-task/page.tsx
+++ app/tasks/new-task/page.tsx
@@ -101,7 +101,7 @@ export default function NewTask() {
               <div className="flex items-center space-x-4">
                 <button
                   type="submit"
-                  className="text-black bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                  className="text-black bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                   Update product
                 </button>
                 <button
