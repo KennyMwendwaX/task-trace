163200a82a78fd3e03d78a11ce6d0b8b3e29bc56
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-04 18:35:35 +0300

    Added select options to the NewTask form

--- app/tasks/new-task/page.tsx
+++ app/tasks/new-task/page.tsx
@@ -2,6 +2,7 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
 import {
   Select,
   SelectContent,
@@ -25,67 +26,76 @@ export default function NewTask() {
           <div className="w-[800px]">
             <form className="space-y-5">
               <div className="sm:col-span-2">
-                <Label htmlFor="title">Title</Label>
+                <Label className="font-semibold" htmlFor="title">
+                  Title
+                </Label>
                 <Input type="title" id="title" placeholder="Title" />
               </div>
               <div className="grid grid-cols-3 gap-3">
                 <div>
-                  <Label htmlFor="select">Label</Label>
+                  <Label className="font-semibold" htmlFor="select">
+                    Label
+                  </Label>
                   <Select>
                     <SelectTrigger>
-                      <SelectValue placeholder="Select a fruit" />
+                      <SelectValue placeholder="Select a Label" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectGroup>
-                        <SelectLabel>Fruits</SelectLabel>
-                        <SelectItem value="apple">Apple</SelectItem>
-                        <SelectItem value="banana">Banana</SelectItem>
-                        <SelectItem value="blueberry">Blueberry</SelectItem>
-                        <SelectItem value="grapes">Grapes</SelectItem>
-                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                        <SelectLabel>Labels</SelectLabel>
+                        <SelectItem value="feature">Feature</SelectItem>
+                        <SelectItem value="documentation">
+                          Documentation
+                        </SelectItem>
+                        <SelectItem value="bug">Bug</SelectItem>
+                        <SelectItem value="error">Error</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
                 </div>
                 <div>
-                  <Label htmlFor="select">Status</Label>
+                  <Label className="font-semibold" htmlFor="select">
+                    Status
+                  </Label>
                   <Select>
                     <SelectTrigger>
-                      <SelectValue placeholder="Select a fruit" />
+                      <SelectValue placeholder="Select a status" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectGroup>
-                        <SelectLabel>Fruits</SelectLabel>
-                        <SelectItem value="apple">Apple</SelectItem>
-                        <SelectItem value="banana">Banana</SelectItem>
-                        <SelectItem value="blueberry">Blueberry</SelectItem>
-                        <SelectItem value="grapes">Grapes</SelectItem>
-                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                        <SelectLabel>Statuses</SelectLabel>
+                        <SelectItem value="done">Done</SelectItem>
+                        <SelectItem value="todo">Todo</SelectItem>
+                        <SelectItem value="inprogress">In Progress</SelectItem>
+                        <SelectItem value="canceled">Canceled</SelectItem>
+                        <SelectItem value="backlog">Backlog</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
                 </div>
                 <div>
-                  <Label htmlFor="select">Priority</Label>
+                  <Label className="font-semibold" htmlFor="select">
+                    Priority
+                  </Label>
                   <Select>
                     <SelectTrigger>
-                      <SelectValue placeholder="Select a fruit" />
+                      <SelectValue placeholder="Select a priority" />
                     </SelectTrigger>
                     <SelectContent>
                       <SelectGroup>
-                        <SelectLabel>Fruits</SelectLabel>
-                        <SelectItem value="apple">Apple</SelectItem>
-                        <SelectItem value="banana">Banana</SelectItem>
-                        <SelectItem value="blueberry">Blueberry</SelectItem>
-                        <SelectItem value="grapes">Grapes</SelectItem>
-                        <SelectItem value="pineapple">Pineapple</SelectItem>
+                        <SelectLabel>Priorities</SelectLabel>
+                        <SelectItem value="high">High</SelectItem>
+                        <SelectItem value="medium">Medium</SelectItem>
+                        <SelectItem value="low">Low</SelectItem>
                       </SelectGroup>
                     </SelectContent>
                   </Select>
                 </div>
               </div>
               <div className="sm:col-span-2">
-                <Label htmlFor="description">Description</Label>
+                <Label className="font-semibold" htmlFor="description">
+                  Description
+                </Label>
                 <Editable
                   className="h-[300px]"
                   editor={editor}
@@ -93,28 +103,7 @@ export default function NewTask() {
                   onChange={setMarkdown}
                 />
               </div>
-              <div className="flex items-center space-x-4">
-                <button
-                  type="submit"
-                  className="text-black bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-                  Update product
-                </button>
-                <button
-                  type="button"
-                  className="text-red-600 inline-flex items-center hover:text-white border border-red-600 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900">
-                  <svg
-                    className="w-5 h-5 mr-1 -ml-1"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      fill-rule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clip-rule="evenodd"></path>
-                  </svg>
-                  Delete
-                </button>
-              </div>{" "}
+              <Button variant="default">Add New Task</Button>
             </form>
           </div>
           <div className="">2</div>
