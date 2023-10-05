cec3b0cd5b8d9b247d651b57bb4e4f6404d3623e
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-05 15:59:46 +0300

    Initialised a dynamic route for one task

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import LatestTasks from "@/components/LatestTasks";
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto mt-4 px-8 pb-5 pt-12">
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
         <h2 className="text-3xl font-bold tracking-tight pb-2">Dashboard</h2>
         <TaskOverview />
         <div className="flex space-x-4 items-start pt-5">
