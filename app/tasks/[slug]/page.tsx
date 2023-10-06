040f3e2e216c7f50f4806240f007c3303ff815d9
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-10-06 17:42:03 +0300

    Added a content layout for the task component

--- app/tasks/[slug]/page.tsx
+++ app/tasks/[slug]/page.tsx
@@ -1,7 +1,43 @@
 export default function page() {
   return (
     <>
-      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">page</div>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-14">
+        <div className="flex items-start">
+          <div className="w-[800px]">
+            <div className="text-2xl font-bold tracking-tight">
+              Prototyping from A to Z: best practices for successful prototypes
+            </div>
+            <div className="flex items-center">
+              <div className="text-sm text-muted-foreground pt-2 pb-5">
+                Created on August 3, 2022, 2:20am
+              </div>
+              <div></div>
+            </div>
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm">
+              <div className="p-3">
+                Successful prototyping relies on a comprehensive approach that
+                encompasses various best practices. Begin by clearly defining
+                your objectives and brainstorming ideas creatively. Choose the
+                right tools and prioritize user experience and usability in your
+                design. Continuously gather feedback from stakeholders and
+                users, securing their support along the way. Highlight core
+                features, iterate as needed, and keep simplicity in mind.
+                Embrace failures as opportunities for improvement and ensure
+                mobile responsiveness and intuitive navigation. Plan your
+                prototyping process well, perform quality checks, use realistic
+                data, and tell a compelling story around your prototype.
+                Extensive user testing, a user-centric approach, and visual
+                consistency are crucial. Start with wireframes, aim for an
+                exceptional user experience, and remember that you are not the
+                end-user. Throughout the process, zero in on your initial goals
+                and objectives, ensuring alignment at every stage.
+              </div>
+            </div>
+          </div>
+
+          <div>2</div>
+        </div>
+      </div>
     </>
   );
 }
