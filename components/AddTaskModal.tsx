c4a875409ca9c4f4e929531af0325f0e52b5fad7
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-01 01:30:12 +0300

    Added db.ts for a single instance of prisma

--- components/AddTaskModal.tsx
+++ components/AddTaskModal.tsx
@@ -1,4 +1,16 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import {
   Dialog,
   DialogContent,
@@ -8,9 +20,26 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { AiOutlinePlus } from "react-icons/ai";
+import { useForm } from "react-hook-form";
 
 export default function AddTaskModal() {
+  const form = useForm();
+
   return (
     <>
       <Dialog>
@@ -26,6 +55,151 @@ export default function AddTaskModal() {
             {/* <DialogDescription>
               Make changes to your profile here. Click save when you are done.
             </DialogDescription> */}
+            <Form {...form}>
+              <form
+                className="space-y-3 px-3"
+                onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="grid md:grid-cols-2 md:gap-6">
+                  <FormField
+                    control={form.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Goal Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            id="name"
+                            className="focus:border-2 focus:border-blue-600"
+                            placeholder="Goal name"
+                            {...field}
+                            required
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="amount"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Goal Amount</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="text"
+                            id="amount"
+                            className="focus:border-2 focus:border-blue-600"
+                            placeholder="Goal amount"
+                            {...field}
+                            required
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <div className="relative">
+                    {/* Use flex to align label and popover content */}
+                    <FormField
+                      control={form.control}
+                      name="target_date"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col">
+                          <FormLabel>Target Date</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "justify-start text-left font-normal",
+                                    !field.value && "text-muted-foreground"
+                                  )}>
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start">
+                              <Calendar
+                                mode="single"
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                initialFocus
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="relative">
+                    <FormField
+                      control={form.control}
+                      name="type"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Goal Type</FormLabel>
+                          <Select
+                            onValueChange={field.onChange}
+                            defaultValue={field.value}
+                            required>
+                            <FormControl>
+                              <SelectTrigger>
+                                <SelectValue placeholder="Select goal type" />
+                              </SelectTrigger>
+                            </FormControl>
+                            <SelectContent>
+                              <SelectItem value="Short Term">
+                                Short Term
+                              </SelectItem>
+                              <SelectItem value="Mid Term">Mid Term</SelectItem>
+                              <SelectItem value="Long Term">
+                                Long Term
+                              </SelectItem>
+                            </SelectContent>
+                          </Select>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Description</FormLabel>
+                        <FormControl>
+                          <Textarea
+                            id="description"
+                            className="focus:border-2 focus:border-blue-600"
+                            placeholder="Goal description"
+                            {...field}
+                            required
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Save Goal</Button>
+                </DialogFooter>
+              </form>
+            </Form>
           </DialogHeader>
         </DialogContent>
       </Dialog>
