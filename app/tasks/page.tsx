e879c9e02412cfa6e5e4343501e0c11263c68e5b
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-01 01:04:44 +0300

    Added the /tasks page

---
+++ app/tasks/page.tsx
@@ -0,0 +1,23 @@
+import { TableColumns } from "@/components/TableColumns";
+import TaskTable from "@/components/TaskTable";
+import { tasks } from "@/data/tasks";
+
+export default function Tasks() {
+  return (
+    <>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <div className="text-2xl font-bold tracking-tight">
+              Welcome back!
+            </div>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks!
+            </p>
+          </div>
+        </div>
+        <TaskTable data={tasks} columns={TableColumns} />
+      </div>
+    </>
+  );
+}
