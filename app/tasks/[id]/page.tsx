fcd64de609cbed07d44542e129c84aa623e1c896
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-02 18:26:15 +0300

    Created /tasks/[id] page.tsx

---
+++ app/tasks/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Task() {
+  return (
+    <>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
+        <div className="text-2xl font-bold tracking-tight">Task</div>
+      </div>
+    </>
+  );
+}
