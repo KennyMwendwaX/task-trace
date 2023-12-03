35203167ae5026f5a076499af740c9f3915ffff2
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-03 20:32:22 +0300

    Replaced IconMappingOptions with taskConfig.ts

--- components/table/TableColumns.tsx
+++ components/table/TableColumns.tsx
@@ -5,7 +5,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { Badge } from "@/components/ui/badge";
 import { Checkbox } from "@/components/ui/checkbox";
 
-import { labels, priorities, statuses } from "@/data/IconMappingOptions";
+import { labels, priorities, statuses } from "@/lib/taskConfig";
 import { Task } from "@/data/schema";
 import TableColumnHeader from "./TableColumnHeader";
 import TableRowActions from "./TableRowActions";
