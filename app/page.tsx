2666131855e15e905655e97bad859c54e60c78dd
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-12 11:43:54 +0300

    Used reat-query to fetch tasks from home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+"use client";
+
 import TaskOverview from "@/components/TaskOverview";
 import TaskChart from "@/components/TaskChart";
 import LatestTasks from "@/components/LatestTasks";
+import { Task } from "@/lib/schema";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await axios.get("/api/tasks");
+      return data.tasks as Task[];
+    },
+  });
+
+  const tasks = data || [];
   return (
     <>
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
         <h2 className="text-3xl font-bold tracking-tight pb-2">Dashboard</h2>
-        <TaskOverview />
+        <TaskOverview tasks={tasks} />
         <div className="flex space-x-4 items-start pt-5">
           <TaskChart />
           <LatestTasks />
