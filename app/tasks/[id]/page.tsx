a57b962bca82bef809645a087b94e2f9d0fd903a
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-22 20:48:17 +0300

    Conditional rendering for task not found

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
+import { taskSchema } from "@/lib/schema";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
 export default function Task({ params }: { params: { slug: string } }) {
   const id = params.slug;
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["tasks"],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/tasks/${id}`);
+      return data.tasks;
+    },
+  });
+
+  const task = taskSchema.parse(data);
+
   return (
     <>
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
-        <div className="text-2xl font-bold tracking-tight">Task</div>
+        {task == null ? (
+          <div className="text-2xl font-bold tracking-tight">
+            Task was not found
+          </div>
+        ) : (
+          <div className="text-2xl font-bold tracking-tight">Task</div>
+        )}
       </div>
     </>
   );
