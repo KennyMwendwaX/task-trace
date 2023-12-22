04690700f1b74d8292ae9f7774dc665a72e5761d
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-22 20:32:54 +0300

    Used /task/[id] page params to access individual id property

--- app/tasks/[id]/page.tsx
+++ app/tasks/[id]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-export default function Task() {
+export default function Task({ params }: { params: { slug: string } }) {
+  const id = params.slug;
   return (
     <>
       <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
