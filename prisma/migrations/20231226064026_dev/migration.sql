e922b272e835ff48cd9909bc6e532b35f538b204
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-26 09:41:23 +0300

    Migrated prisma schema to db

--- prisma/migrations/20231226064026_dev/migration.sql
+++ prisma/migrations/20231226064026_dev/migration.sql
@@ -1,11 +1,12 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "name" TEXT,
-    "email" TEXT,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "emailVerified" TIMESTAMP(3),
-    "password" TEXT,
+    "password" TEXT NOT NULL,
     "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
