7b44fa11799a52da9a92d9d852dfc489121d8305
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-27 09:01:32 +0300

    Removed searchbox on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,6 +22,7 @@ import {
 import { LuActivity } from "react-icons/lu";
 import { FiCheckCircle } from "react-icons/fi";
 import Sidebar from "./Sidebar";
+import { MdLogout } from "react-icons/md";
 
 export default function Navbar() {
   const pathname = usePathname();
@@ -29,92 +30,69 @@ export default function Navbar() {
   return (
     <>
       <nav className="bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-600">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-2">
-          <div className="flex items-center space-x-8">
+        <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-2">
+          <div className="flex items-center space-x-2">
             <Sidebar />
-            <Link className="flex items-center space-x-1" href="/">
-              <FiCheckCircle className="text-slate-200 w-7 h-7" />
-              <span className="self-center text-xl font-semibold whitespace-nowrap text-slate-200">
-                TaskTracker
-              </span>
-            </Link>
-            <div className="space-x-3">
-              <Link
-                href="/dashboard"
-                className={`${
-                  pathname === "/dashboard"
-                    ? "text-gray-100"
-                    : "text-gray-400 hover:text-gray-200"
-                } text-sm font-medium`}>
-                Dashboard
+            <div className="flex items-center space-x-8">
+              <Link className="flex items-center space-x-1" href="/">
+                <FiCheckCircle className="text-slate-200 w-7 h-7" />
+                <span className="self-center text-xl font-semibold whitespace-nowrap text-slate-200">
+                  TaskTracker
+                </span>
               </Link>
-              <Link
-                href="/tasks"
-                className={`${
-                  pathname === "/tasks"
-                    ? "text-gray-100"
-                    : "text-gray-400 hover:text-gray-200"
-                } text-sm font-medium`}>
-                Tasks
-              </Link>
+              <div className="space-x-3">
+                <Link
+                  href="/dashboard"
+                  className={`${
+                    pathname === "/dashboard"
+                      ? "text-gray-100"
+                      : "text-gray-400 hover:text-gray-200"
+                  } text-sm font-medium`}>
+                  Dashboard
+                </Link>
+                <Link
+                  href="/tasks"
+                  className={`${
+                    pathname === "/tasks"
+                      ? "text-gray-100"
+                      : "text-gray-400 hover:text-gray-200"
+                  } text-sm font-medium`}>
+                  Tasks
+                </Link>
+              </div>
             </div>
           </div>
           <div className="flex md:order-2 items-center space-x-3">
-            <div className="relative hidden md:block">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
-                <MagnifyingGlassIcon className="text-gray-400 w-5 h-5" />
-                <span className="sr-only">Search icon</span>
-              </div>
-              <input
-                type="text"
-                id="search-navbar"
-                className="block w-full outline-none p-2 pl-8 text-sm border rounded-lg bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
-                placeholder="Search..."
-              />
-            </div>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
                   variant="ghost"
                   className="relative h-8 w-8 rounded-full">
                   <Avatar className="h-8 w-8">
-                    {/* <AvatarImage src="/avatars/01.png" alt="@shadcn" /> */}
+                    <AvatarImage src={""} alt="profile-image" />
                     <AvatarFallback>
                       <PersonIcon className="h-5 w-5" />
                     </AvatarFallback>
                   </Avatar>
-                  <span className="sr-only">User button</span>
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56" align="end" forceMount>
                 <DropdownMenuLabel className="font-normal">
                   <div className="flex flex-col space-y-1">
-                    <p className="text-sm font-medium leading-none">shadcn</p>
+                    <p className="text-base font-medium leading-none">
+                      John Doe
+                    </p>
                     <p className="text-xs leading-none text-muted-foreground">
-                      m@example.com
+                      johndoe@gmail.com
                     </p>
                   </div>
                 </DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem>
-                    Profile
-                    <DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    Billing
-                    <DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    Settings
-                    <DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>New Team</DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
                 <DropdownMenuItem>
-                  Log out
-                  <DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
+                  <button className="flex items-center">
+                    <MdLogout className="mr-1 w-4 h-4" />
+                    Log out
+                  </button>
                 </DropdownMenuItem>
               </DropdownMenuContent>
             </DropdownMenu>
