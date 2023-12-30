05db181e04eab884e0ed6c4f7f43986f4082bcd5
kenny-mwendwa <kennymwendwa67@gmail.com>
2023-12-30 07:51:47 +0300

    Added TableColumns for user table

--- components/tables/UserTable/TableColumns.tsx
+++ components/tables/UserTable/TableColumns.tsx
@@ -4,7 +4,7 @@ import { ColumnDef } from "@tanstack/react-table";
 
 import { Checkbox } from "@/components/ui/checkbox";
 
-import { User } from "@/lib/schema/UserSchema";
+import { User, userSchema } from "@/lib/schema/UserSchema";
 import TableColumnHeader from "./TableColumnHeader";
 
 export const TableColumns: ColumnDef<User>[] = [
@@ -28,5 +28,64 @@ export const TableColumns: ColumnDef<User>[] = [
     ),
     enableSorting: false,
     enableHiding: false,
+  },
+  {
+    accessorKey: "member",
+    header: () => <TableColumnHeader name="Member" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      return <div>{member.name}</div>;
+    },
+  },
+  {
+    accessorKey: "tasks",
+    header: () => <TableColumnHeader name="Tasks" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      const tasks = member.tasks.length;
+      return <div>{tasks}</div>;
+    },
+  },
+  {
+    accessorKey: "done",
+    header: () => <TableColumnHeader name="Done" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      const tasks = member.tasks;
+      const tasksDone = tasks.filter((task) => task.status === "DONE");
+      return <div>{tasksDone.length}</div>;
+    },
+  },
+  {
+    accessorKey: "todo",
+    header: () => <TableColumnHeader name="Todo" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      const tasks = member.tasks;
+      const tasksTodo = tasks.filter((task) => task.status === "TO_DO");
+      return <div>{tasksTodo.length}</div>;
+    },
+  },
+  {
+    accessorKey: "inprogress",
+    header: () => <TableColumnHeader name="In Progress" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      const tasks = member.tasks;
+      const tasksInProgress = tasks.filter(
+        (task) => task.status === "IN_PROGRESS"
+      );
+      return <div>{tasksInProgress.length}</div>;
+    },
+  },
+  {
+    accessorKey: "canceled",
+    header: () => <TableColumnHeader name="Canceled" />,
+    cell: ({ row }) => {
+      const member = userSchema.parse(row.original);
+      const tasks = member.tasks;
+      const tasksCanceled = tasks.filter((task) => task.status === "CANCELED");
+      return <div>{tasksCanceled.length}</div>;
+    },
   },
 ];
