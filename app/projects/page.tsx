caf80ef810a33f6c34fbc312fa74998c46765dc2
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-01 22:53:34 +0300

    Added /projects page.tsx

---
+++ app/projects/page.tsx
@@ -0,0 +1,9 @@
+export default function Projects() {
+  return (
+    <>
+      <div className="container mx-auto mt-4 px-12 pb-5 pt-12">
+        <div>Projects</div>
+      </div>
+    </>
+  );
+}
