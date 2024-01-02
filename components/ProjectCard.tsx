b0285717a2cb22089cb44cca9a35a51da54c39f6
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-02 11:34:09 +0300

    Added ProjectCard.tsx component

---
+++ components/ProjectCard.tsx
@@ -0,0 +1,60 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { IoChevronForward } from "react-icons/io5";
+import { LuUser2 } from "react-icons/lu";
+
+export default function ProjectCard() {
+  return (
+    <>
+      <Card>
+        <CardHeader className="space-y-2">
+          <h2 className="font-semibold text-lg">Project Alpha</h2>
+          <div className="flex items-center space-x-1">
+            <Avatar>
+              <AvatarImage src={""} />
+              <AvatarFallback>
+                <LuUser2 className="w-5 h-5" />
+              </AvatarFallback>
+            </Avatar>
+            <div>John Doe</div>
+          </div>
+        </CardHeader>
+        <CardContent className="space-y-2">
+          <Badge
+            variant="outline"
+            className="border-orange-600 text-orange-500">
+            In Progress
+          </Badge>
+          <p className="text-sm text-gray-500">
+            A new mobile app for the customer platform.
+          </p>
+          <p className="text-sm text-gray-500">Task Completion Rate: 35/50</p>
+          <div className="w-full h-2 bg-gray-200 rounded-full">
+            <div
+              className="h-full text-xs text-center text-white bg-blue-500 rounded-full"
+              style={{
+                width: "70%",
+              }}
+            />
+            <p className="text-xs text-gray-500 text-right">70% Complete</p>
+          </div>
+        </CardContent>
+        <CardFooter className="flex justify-end">
+          <Link
+            href="#"
+            className="flex items-center cursor-pointer text-sm text-blue-800 hover:underline">
+            <span>View project</span>
+            <IoChevronForward className="w-3 h-3" />
+          </Link>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
