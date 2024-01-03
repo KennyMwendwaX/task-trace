8cf76734b6723ab06b872127fa22950c98368691
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-03 07:09:19 +0300

    npm i next-auth @auth/prisma-adapter

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,17 +1,50 @@
 import prisma from "@/prisma/db";
 import { NextResponse } from "next/server";
+import { projectFormSchema } from "@/lib/schema/ProjectSchema";
 
 export async function GET() {
   try {
     const projects = await prisma.project.findMany();
 
-    if (!projects)
+    if (projects.length === 0)
       return NextResponse.json(
         { message: "No projects found" },
         { status: 404 }
       );
 
     return NextResponse.json({ projects }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error, try again later" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: Request) {
+  const req = await request.json();
+
+  const requestData = {
+    ...req,
+    start_date: new Date(req.start_date),
+    end_date: new Date(req.end_date),
+  };
+
+  const validatedData = projectFormSchema.parse(requestData);
+
+  if (!validatedData)
+    return NextResponse.json(
+      { message: "Invalid request data" },
+      { status: 400 }
+    );
+
+  try {
+    const project = await prisma.project.create({
+      data: {
+        ...validatedData,
+        status: "TO_DO",
+      },
+    });
   } catch (error) {
     return NextResponse.json(
       { message: "Server error, try again later" },
--- app/api/user/tasks/route.ts
+++ app/api/user/tasks/route.ts
@@ -6,10 +6,7 @@ export async function GET() {
   try {
     const tasks = await prisma.task.findMany();
     if (tasks.length === 0)
-      return NextResponse.json(
-        { message: "Failed to return tasks" },
-        { status: 404 }
-      );
+      return NextResponse.json({ message: "No tasks found" }, { status: 404 });
 
     return NextResponse.json({ tasks }, { status: 200 });
   } catch (error) {
@@ -24,13 +21,13 @@ export async function POST(request: Request) {
   const req = await request.json();
 
   // Convert 'due_date' string to a Date object
-  const parsedData = {
+  const requestData = {
     ...req,
     due_date: new Date(req.due_date),
   };
 
   // Apply Zod schema to validate and parse the request data
-  const validatedData = taskFormSchema.parse(parsedData);
+  const validatedData = taskFormSchema.parse(requestData);
 
   if (!validatedData)
     return NextResponse.json(
