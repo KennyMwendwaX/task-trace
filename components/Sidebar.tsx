5be78d709b683c599f189ecfb1f48fae4bab8353
kenny-mwendwa <kennymwendwa67@gmail.com>
2024-01-07 16:16:45 +0300

    Added links to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,20 +17,20 @@ export default function Sidebar() {
         <div className="flex-1 justify-center pt-[80px] py-2">
           <nav className="grid items-start px-4 text-sm font-medium">
             <Link
-              className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#">
+              href="/home"
+              className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
               <HomeIcon className="h-4 w-4" />
               Home
             </Link>
             <Link
-              className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2.5 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-              href="#">
+              href="/projects"
+              className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2.5 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50">
               <ProjectorIcon className="h-4 w-4" />
               Projects
             </Link>
             <Link
-              className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-              href="#">
+              href="/team"
+              className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50">
               <UsersIcon className="h-4 w-4" />
               Team
             </Link>
