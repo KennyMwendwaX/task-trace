f43eb98e72fd94fc2e328c23f1d91c7ad457faa1
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-02 11:33:28 +0300

    refactor: replaced tailwind-scrollbar with shadcn/ui ScrollArea in the AddTask form modal

--- components/AddTaskModal.tsx
+++ components/AddTaskModal.tsx
@@ -54,6 +54,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { Member } from "@/lib/schema/UserSchema";
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
+import { ScrollArea } from "./ui/scroll-area";
 
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
@@ -114,51 +115,30 @@ export default function AddTaskModal({ projectId, members }: Props) {
             <span>New Task</span>
           </Button>
         </DialogTrigger>
-        <DialogContent className="max-w-2xl max-h-[80vh] overflow-y-auto scrollbar-w-2 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-thumb-gray-400 scrollbar-track-white">
+        <DialogContent className="max-w-2xl">
           <DialogHeader>
             <DialogTitle>New Task</DialogTitle>
             {/* <DialogDescription>
               Make changes to your profile here. Click save when you are done.
             </DialogDescription> */}
 
-            <Form {...form}>
-              <form
-                className="space-y-3 pt-4 px-3"
-                onSubmit={form.handleSubmit(onSubmit)}>
-                <FormField
-                  control={form.control}
-                  name="name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Task Name</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="text"
-                          id="name"
-                          className="focus:border-2 focus:border-blue-600"
-                          placeholder="Task name"
-                          {...field}
-                          required
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <div className="grid md:grid-cols-2 md:gap-6">
+            <ScrollArea className="h-[80vh]">
+              <Form {...form}>
+                <form
+                  className="space-y-3 pt-4 px-3"
+                  onSubmit={form.handleSubmit(onSubmit)}>
                   <FormField
                     control={form.control}
-                    name="label"
+                    name="name"
                     render={({ field }) => (
                       <FormItem>
-                        <FormLabel>Task Label</FormLabel>
+                        <FormLabel>Task Name</FormLabel>
                         <FormControl>
                           <Input
                             type="text"
-                            id="label"
+                            id="name"
                             className="focus:border-2 focus:border-blue-600"
-                            placeholder="Task label"
+                            placeholder="Task name"
                             {...field}
                             required
                           />
@@ -168,70 +148,149 @@ export default function AddTaskModal({ projectId, members }: Props) {
                     )}
                   />
 
-                  <div className="relative">
+                  <div className="grid md:grid-cols-2 md:gap-6">
                     <FormField
                       control={form.control}
-                      name="priority"
+                      name="label"
                       render={({ field }) => (
                         <FormItem>
-                          <FormLabel>Task Priority</FormLabel>
-                          <Select
-                            onValueChange={field.onChange}
-                            defaultValue={field.value}
-                            required>
-                            <FormControl>
-                              <SelectTrigger>
-                                <SelectValue placeholder="Select priority type" />
-                              </SelectTrigger>
-                            </FormControl>
-                            <SelectContent>
-                              <SelectItem value="LOW">Low</SelectItem>
-                              <SelectItem value="MEDIUM">Medium</SelectItem>
-                              <SelectItem value="HIGH">High</SelectItem>
-                            </SelectContent>
-                          </Select>
+                          <FormLabel>Task Label</FormLabel>
+                          <FormControl>
+                            <Input
+                              type="text"
+                              id="label"
+                              className="focus:border-2 focus:border-blue-600"
+                              placeholder="Task label"
+                              {...field}
+                              required
+                            />
+                          </FormControl>
                           <FormMessage />
                         </FormItem>
                       )}
                     />
-                  </div>
 
-                  <div className="relative">
-                    {/* Use flex to align label and popover content */}
+                    <div className="relative">
+                      <FormField
+                        control={form.control}
+                        name="priority"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Task Priority</FormLabel>
+                            <Select
+                              onValueChange={field.onChange}
+                              defaultValue={field.value}
+                              required>
+                              <FormControl>
+                                <SelectTrigger>
+                                  <SelectValue placeholder="Select priority type" />
+                                </SelectTrigger>
+                              </FormControl>
+                              <SelectContent>
+                                <SelectItem value="LOW">Low</SelectItem>
+                                <SelectItem value="MEDIUM">Medium</SelectItem>
+                                <SelectItem value="HIGH">High</SelectItem>
+                              </SelectContent>
+                            </Select>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
+
+                    <div className="relative">
+                      {/* Use flex to align label and popover content */}
+                      <FormField
+                        control={form.control}
+                        name="due_date"
+                        render={({ field }) => (
+                          <FormItem className="flex flex-col">
+                            <FormLabel>Due Date</FormLabel>
+                            <Popover>
+                              <PopoverTrigger asChild>
+                                <FormControl>
+                                  <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                      "justify-start text-left font-normal",
+                                      !field.value && "text-muted-foreground"
+                                    )}>
+                                    {field.value ? (
+                                      format(new Date(field.value), "PPP")
+                                    ) : (
+                                      <span>Pick a date</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                  </Button>
+                                </FormControl>
+                              </PopoverTrigger>
+                              <PopoverContent
+                                className="w-auto p-0"
+                                align="start">
+                                <Calendar
+                                  mode="single"
+                                  selected={new Date(field.value)}
+                                  onSelect={field.onChange}
+                                  initialFocus
+                                  disabled={(date) => date <= new Date()}
+                                />
+                              </PopoverContent>
+                            </Popover>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                    </div>
                     <FormField
                       control={form.control}
-                      name="due_date"
+                      name="memberId"
                       render={({ field }) => (
                         <FormItem className="flex flex-col">
-                          <FormLabel>Due Date</FormLabel>
+                          <FormLabel>Assign Task</FormLabel>
                           <Popover>
                             <PopoverTrigger asChild>
                               <FormControl>
                                 <Button
-                                  variant={"outline"}
+                                  variant="outline"
+                                  role="combobox"
                                   className={cn(
-                                    "justify-start text-left font-normal",
+                                    "justify-between",
                                     !field.value && "text-muted-foreground"
                                   )}>
-                                  {field.value ? (
-                                    format(new Date(field.value), "PPP")
-                                  ) : (
-                                    <span>Pick a date</span>
-                                  )}
-                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                  {field.value
+                                    ? members.find(
+                                        (member) => member.id === field.value
+                                      )?.userName
+                                    : "Select member"}
+                                  <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
                                 </Button>
                               </FormControl>
                             </PopoverTrigger>
-                            <PopoverContent
-                              className="w-auto p-0"
-                              align="start">
-                              <Calendar
-                                mode="single"
-                                selected={new Date(field.value)}
-                                onSelect={field.onChange}
-                                initialFocus
-                                disabled={(date) => date <= new Date()}
-                              />
+                            <PopoverContent className="w-[200px] p-0">
+                              <Command>
+                                <CommandInput placeholder="Search member" />
+                                <CommandEmpty>No member found.</CommandEmpty>
+                                <CommandGroup>
+                                  {members.map((member) => (
+                                    <CommandItem
+                                      value={member.userName}
+                                      key={member.id}
+                                      onSelect={() => {
+                                        form.setValue("memberId", member.id);
+                                      }}>
+                                      <Check
+                                        className={cn(
+                                          "mr-2 h-4 w-4",
+                                          member.id === field.value
+                                            ? "opacity-100"
+                                            : "opacity-0"
+                                        )}
+                                      />
+                                      {member.userName}
+                                    </CommandItem>
+                                  ))}
+                                </CommandGroup>
+                              </Command>
                             </PopoverContent>
                           </Popover>
                           <FormMessage />
@@ -239,84 +298,27 @@ export default function AddTaskModal({ projectId, members }: Props) {
                       )}
                     />
                   </div>
-                  <FormField
-                    control={form.control}
-                    name="memberId"
-                    render={({ field }) => (
-                      <FormItem className="flex flex-col">
-                        <FormLabel>Assign Task</FormLabel>
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <FormControl>
-                              <Button
-                                variant="outline"
-                                role="combobox"
-                                className={cn(
-                                  "justify-between",
-                                  !field.value && "text-muted-foreground"
-                                )}>
-                                {field.value
-                                  ? members.find(
-                                      (member) => member.id === field.value
-                                    )?.userName
-                                  : "Select member"}
-                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-                              </Button>
-                            </FormControl>
-                          </PopoverTrigger>
-                          <PopoverContent className="w-[200px] p-0">
-                            <Command>
-                              <CommandInput placeholder="Search member" />
-                              <CommandEmpty>No member found.</CommandEmpty>
-                              <CommandGroup>
-                                {members.map((member) => (
-                                  <CommandItem
-                                    value={member.userName}
-                                    key={member.id}
-                                    onSelect={() => {
-                                      form.setValue("memberId", member.id);
-                                    }}>
-                                    <Check
-                                      className={cn(
-                                        "mr-2 h-4 w-4",
-                                        member.id === field.value
-                                          ? "opacity-100"
-                                          : "opacity-0"
-                                      )}
-                                    />
-                                    {member.userName}
-                                  </CommandItem>
-                                ))}
-                              </CommandGroup>
-                            </Command>
-                          </PopoverContent>
-                        </Popover>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                <div>
-                  <FormField
-                    control={form.control}
-                    name="description"
-                    render={({ field }) => (
-                      <FormItem>
-                        <FormLabel>Description</FormLabel>
-                        <FormControl>
-                          <SimpleMDE
-                            id="description"
-                            className="focus:border-2 focus:border-blue-600"
-                            placeholder="Task description"
-                            {...field}
-                          />
-                        </FormControl>
-                        <FormMessage />
-                      </FormItem>
-                    )}
-                  />
-                </div>
-                {/* <div>
+                  <div>
+                    <FormField
+                      control={form.control}
+                      name="description"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Description</FormLabel>
+                          <FormControl>
+                            <SimpleMDE
+                              id="description"
+                              className="focus:border-2 focus:border-blue-600"
+                              placeholder="Task description"
+                              {...field}
+                            />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  {/* <div>
                   <FormField
                     control={form.control}
                     name="description"
@@ -337,11 +339,12 @@ export default function AddTaskModal({ projectId, members }: Props) {
                     )}
                   />
                 </div> */}
-                <DialogFooter>
-                  <Button type="submit">Save Task</Button>
-                </DialogFooter>
-              </form>
-            </Form>
+                  <DialogFooter>
+                    <Button type="submit">Save Task</Button>
+                  </DialogFooter>
+                </form>
+              </Form>
+            </ScrollArea>
           </DialogHeader>
         </DialogContent>
       </Dialog>
