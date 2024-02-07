c57b0069d31d246ee8fc754a6150d2b8fed49eb2
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-07 08:51:16 +0300

    feat: add DELETE api route /api/projects/[projectId]/members/[memberId]

--- app/api/projects/[projectId]/members/[memberId]/route.ts
+++ app/api/projects/[projectId]/members/[memberId]/route.ts
@@ -34,3 +34,37 @@ export async function GET(
     );
   }
 }
+
+export async function DELETE(
+  request: Request,
+  { params }: { params: { memberId: string } }
+) {
+  const memberId = params.memberId;
+
+  if (!memberId)
+    return NextResponse.json(
+      { message: "No memberId provided" },
+      { status: 400 }
+    );
+
+  try {
+    const deletedMember = await prisma.member.delete({
+      where: {
+        id: memberId,
+      },
+    });
+
+    if (!deletedMember)
+      return NextResponse.json(
+        { message: "Failed to delete member" },
+        { status: 500 }
+      );
+
+    return NextResponse.json({ message: "Member deleted" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Server error, try again later" },
+      { status: 500 }
+    );
+  }
+}
