58d29635549a62e1855cdd004ecabe1124fe76f9
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-09 07:42:12 +0300

    refactor: remove data fields in member model and task model to reduce redundancy

--- components/tables/MemberTable/MemberTable.tsx
+++ components/tables/MemberTable/MemberTable.tsx
@@ -83,7 +83,8 @@ export default function MemberTable<TData, TValue>({
 
   const csvData = members.map((member) => ({
     name: member.userName,
-    email: member.userId,
+    email: member.email,
+    role: member.role,
     tasks: member.tasks.length,
     tasksDone: getTasksByStatus(member, "DONE"),
     tasksTodo: getTasksByStatus(member, "TO_DO"),
@@ -94,6 +95,7 @@ export default function MemberTable<TData, TValue>({
   const headers = [
     { label: "Member", key: "name" },
     { label: "Email", key: "email" },
+    { label: "Role", key: "role" },
     { label: "Tasks Done", key: "tasksDone" },
     { label: "Tasks Todo", key: "tasksTodo" },
     { label: "Tasks In Progress", key: "tasksInProgress" },
