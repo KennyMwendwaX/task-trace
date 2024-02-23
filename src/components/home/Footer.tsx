a09ccd53be6943ce6357ad521e73014fdb55ebd2
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-23 07:48:52 +0300

    feat: add Footer.tsx Home component

---
+++ src/components/home/Footer.tsx
@@ -0,0 +1,184 @@
+import { Input } from "@/components/ui/input";
+import { FaGithub, FaLinkedinIn, FaXTwitter } from "react-icons/fa6";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        {/* Top area: Blocks */}
+        <div className="grid sm:grid-cols-12 gap-16 py-8 md:py-12 border-t border-gray-200">
+          {/* 1st block */}
+          <div className="sm:col-span-12 lg:col-span-3">
+            <div className="mb-2 self-center text-3xl font-semibold whitespace-nowrap text-slate-800">
+              PesaIQ
+            </div>
+          </div>
+
+          {/* 2nd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Resources</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Documentation
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Tutorials & Guides
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Blog
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Support Center
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Partners
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* 3rd block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-2">
+            <h6 className="text-gray-800 font-medium mb-2">Company</h6>
+            <ul className="text-sm">
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Home
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  About us
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Company values
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Pricing
+                </a>
+              </li>
+              <li className="mb-2">
+                <a
+                  href="#0"
+                  className="text-gray-600 hover:text-gray-900 transition duration-150 ease-in-out">
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          {/* 4th block */}
+          <div className="sm:col-span-6 md:col-span-3 lg:col-span-3">
+            <h6 className="text-gray-800 font-medium mb-2">Subscribe</h6>
+            <p className="text-sm text-gray-600 mb-4">
+              Get the latest news and articles to your inbox every month.
+            </p>
+            <form>
+              <div className="flex flex-wrap mb-4">
+                <div className="w-full">
+                  <label className="block text-sm sr-only" htmlFor="newsletter">
+                    Email
+                  </label>
+                  <div className="relative flex items-center max-w-xs">
+                    <Input
+                      className="focus:border-2 focus:border-blue-500"
+                      id="newsletter"
+                      type="email"
+                      placeholder="youremail@gmail.com"
+                    />
+                    <button
+                      type="submit"
+                      className="absolute inset-0 left-auto"
+                      aria-label="Subscribe">
+                      <span
+                        className="absolute inset-0 right-auto w-px -ml-px my-2 bg-gray-300"
+                        aria-hidden="true"></span>
+                      <svg
+                        className="w-3 h-3 fill-current text-blue-600 mx-3 shrink-0"
+                        viewBox="0 0 12 12"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z"
+                          fillRule="nonzero"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                  {/* Success message */}
+                  {/* <p className="mt-2 text-green-600 text-sm">Thanks for subscribing!</p> */}
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* Bottom area */}
+        <div className="md:flex md:items-center md:justify-between py-4 md:py-8 border-t border-gray-200">
+          {/* Social as */}
+          <ul className="flex mb-4 md:order-1 md:ml-4 md:mb-0">
+            <li>
+              <a
+                href="#0"
+                className="flex justify-center p-2.5 items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label="Twitter">
+                <FaXTwitter className="w-5 h-5" />
+              </a>
+            </li>
+            <li className="ml-4">
+              <a
+                href="#0"
+                className="flex justify-center p-2.5 items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label="Github">
+                <FaGithub className="w-5 h-5" />
+              </a>
+            </li>
+            <li className="ml-4">
+              <a
+                href="#0"
+                className="flex justify-center p-2.5 items-center text-gray-600 hover:text-gray-900 bg-white hover:bg-white-100 rounded-full shadow transition duration-150 ease-in-out"
+                aria-label="LinkedIn">
+                <FaLinkedinIn className="w-5 h-5" />
+              </a>
+            </li>
+          </ul>
+
+          {/* Copyrights note */}
+          <div className="text-sm text-gray-600 mr-4">
+            &copy; PesaIQ.com. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
