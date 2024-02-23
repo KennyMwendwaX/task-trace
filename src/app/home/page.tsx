f99b2023ee20a41b0a392c4170651cc2d331a2d9
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-02-23 07:55:10 +0300

    feat: add Home2 page component

---
+++ src/app/home/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/components/home/Header";
+import Hero from "@/components/home/Hero";
+import Features from "@/components/home/Features";
+import Footer from "@/components/home/Footer";
+
+export default function Home2() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Features />
+      <Footer />
+    </>
+  );
+}
