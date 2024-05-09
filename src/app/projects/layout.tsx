cd57af12d080ce81a972ea425dd3714a07fce903
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-05-09 21:26:04 +0300

    feat: move layout from /projects dir to /projects/[projectId]

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -1,125 +1,14 @@
 import type { Metadata } from "next";
-import Link from "next/link";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import {
-  Bell,
-  CircleUser,
-  Home,
-  LineChart,
-  Menu,
-  Package,
-  Package2,
-  Search,
-  ShoppingCart,
-  Users,
-} from "lucide-react";
-import { Badge } from "@/components/ui/badge";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { PersonIcon } from "@radix-ui/react-icons";
-import Logo from "../../../public/logo.png";
-import { IoSettingsOutline } from "react-icons/io5";
-import { MdLogout } from "react-icons/md";
-import ProjectNavbar from "@/components/ProjectNavbar";
 
 export const metadata: Metadata = {
   title: "Projects",
   description: "Projects page",
 };
 
-export default function ProjectLayout({
+export default function ProjectsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <>
-      <ProjectNavbar />
-      {/* Sidebar */}
-      <div className="fixed top-0 left-0 w-[260px] h-full border-r bg-muted/40 hidden lg:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex-1 mt-[72px]">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-muted-foreground transition-all hover:text-primary">
-                <Home className="h-4 w-4" />
-                Dashboard
-              </Link>
-              {/* Arrow Home*/}
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-muted-foreground transition-all hover:text-primary">
-                <Home className="h-4 w-4" />
-                Overview
-              </Link>
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-muted-foreground transition-all hover:text-primary">
-                <ShoppingCart className="h-4 w-4" />
-                Projects
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                  6
-                </Badge>
-              </Link>
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2.5 text-primary transition-all hover:text-primary">
-                <Package className="h-4 w-4" />
-                Tasks{" "}
-              </Link>
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-muted-foreground transition-all hover:text-primary">
-                <Users className="h-4 w-4" />
-                Members
-              </Link>
-              <Link
-                href="#"
-                className="flex items-center gap-3 rounded-lg px-3 py-2.5 text-muted-foreground transition-all hover:text-primary">
-                <LineChart className="h-4 w-4" />
-                Analytics
-              </Link>
-            </nav>
-          </div>
-          <div className="mt-auto p-4">
-            <Card x-chunk="dashboard-02-chunk-0">
-              <CardHeader className="p-2 pt-0 md:p-4">
-                <CardTitle>Upgrade to Pro</CardTitle>
-                <CardDescription>
-                  Unlock all features and get unlimited access to our support
-                  team.
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="p-2 pt-0 md:p-4 md:pt-0">
-                <Button size="sm" className="w-full">
-                  Upgrade
-                </Button>
-              </CardContent>
-            </Card>
-          </div>
-        </div>
-      </div>
-      {children}
-    </>
-  );
+  return <>{children}</>;
 }
