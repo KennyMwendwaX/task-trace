5fea98632b24f34e585089bc514d79873fcb69df
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-05-13 08:54:04 +0300

    feat: render piined and other projects in the projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,8 +8,6 @@ import { Project } from "@/lib/schema/ProjectSchema";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { LuFolders, LuPin, LuSearch } from "react-icons/lu";
-import { HiMiniRectangleStack } from "react-icons/hi2";
-import { MdOutlineAddTask } from "react-icons/md";
 
 export default function Projects() {
   const { data, isLoading, error } = useQuery({
@@ -36,66 +34,78 @@ export default function Projects() {
 
   return (
     <>
-      {/* {isLoading ? (
-        <Loading />
-      ) : projects.length > 0 ? (
+      {pinnedProjects.length > 0 || otherProjects.length > 0 ? (
         <>
-          <div className="flex items-center justify-between">
-            <h1 className="font-semibold text-lg md:text-2xl">Projects</h1>
+          <div className="flex items-center justify-between sm:gap-2">
+            <div className="relative md:grow-0">
+              <LuSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search projects..."
+                className="w-[200px] rounded-lg bg-background pl-8 md:w-[345px] lg:w-[400px]"
+              />
+            </div>
             <AddProjectModal />
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {projects.map((project) => (
-              <ProjectCard key={project.id} project={project} />
-            ))}
-          </div>
+          {pinnedProjects.length > 0 ? (
+            <>
+              <div className="flex items-center mt-5">
+                <LuPin className="mr-2 w-7 h-7" />
+                <span className="text-xl font-semibold">Pinned Projects</span>
+              </div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5 mt-4">
+                {pinnedProjects.map((project) => (
+                  <ProjectCard key={project.id} project={project} />
+                ))}
+              </div>
+              {otherProjects.length > 0 ? (
+                <div className="mt-6">
+                  <div className="flex items-center">
+                    <LuFolders className="mr-2 w-7 h-7" />
+                    <span className="text-xl font-semibold">
+                      Other Projects
+                    </span>
+                  </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5 mt-4">
+                    {otherProjects.map((project) => (
+                      <ProjectCard key={project.id} project={project} />
+                    ))}
+                  </div>
+                </div>
+              ) : null}
+            </>
+          ) : (
+            <>
+              <div className="flex items-center justify-between sm:gap-2">
+                <div className="relative md:grow-0">
+                  <LuSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                  <Input
+                    type="search"
+                    placeholder="Search projects..."
+                    className="w-[200px] rounded-lg bg-background pl-8 md:w-[345px] lg:w-[400px]"
+                  />
+                </div>
+                <AddProjectModal />
+              </div>
+              {otherProjects.length > 0 ? (
+                <>
+                  <div className="flex items-center mt-5">
+                    <LuPin className="mr-2 w-7 h-7" />
+                    <span className="text-xl font-semibold">Projects</span>
+                  </div>
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5 mt-4">
+                    {otherProjects.map((project) => (
+                      <ProjectCard key={project.id} project={project} />
+                    ))}
+                  </div>
+                </>
+              ) : null}
+            </>
+          )}
         </>
       ) : (
-        <div className="mx-auto flex max-w-[420px] flex-col items-center justify-center text-center pt-36">
-          <MdOutlineAddTask className="h-14 w-14 text-muted-foreground" />
-
-          <h3 className="mt-4 text-2xl font-semibold">No projects added</h3>
-          <p className="mb-4 mt-2 text-lg text-muted-foreground">
-            You have not added any projects. Add one below.
-          </p>
-          <AddProjectModal />
-        </div>
-      )} */}
-      <div className="flex items-center justify-between sm:gap-2">
-        <div className="relative md:grow-0">
-          <LuSearch className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-          <Input
-            type="search"
-            placeholder="Search projects..."
-            className="w-[200px] rounded-lg bg-background pl-8 md:w-[345px] lg:w-[400px]"
-          />
-        </div>
-        <AddProjectModal />
-      </div>
-
-      <div className="flex items-center mt-5">
-        <LuPin className="mr-2 w-7 h-7" />
-        <span className="text-xl font-semibold">Pinned Projects</span>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5 mt-4">
-        <ProjectCard />
-        <ProjectCard />
-      </div>
-
-      <div className="mt-6">
-        <div className="flex items-center">
-          <LuFolders className="mr-2 w-7 h-7" />
-          <span className="text-xl font-semibold">Other Projects</span>
-        </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 lg:gap-5 mt-4">
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
-        </div>
-      </div>
+        <div>No project found</div>
+      )}
     </>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -21,7 +21,7 @@ import { Project } from "@/lib/schema/ProjectSchema";
 import Link from "next/link";
 import { IoChevronForward } from "react-icons/io5";
 import { LuMoreVertical, LuTruck, LuUser2 } from "react-icons/lu";
-import { statuses } from "@/lib/config";
+import { projectStatuses } from "@/lib/config";
 import format from "date-fns/format";
 import Image from "next/image";
 import Logo from "../../public/logo.png";
@@ -30,87 +30,33 @@ interface Props {
   project: Project;
 }
 
-// export default function ProjectCard({ project }: Props) {
-// const projectStatus = statuses.find(
-//   (status) => status.value === project.status
-// );
-export default function ProjectCard() {
+export default function ProjectCard({ project }: Props) {
+  const projectStatus = projectStatuses.find(
+    (status) => status.value === project.status
+  );
   return (
     <>
-      {/* <Card>
-        <CardHeader className="space-y-2">
-          <h2 className="font-semibold text-lg">{project.name}</h2>
-          <div className="flex items-center space-x-1">
-            <Avatar>
-              <AvatarImage src={""} />
-              <AvatarFallback>
-                <LuUser2 className="w-5 h-5" />
-              </AvatarFallback>
-            </Avatar>
-            <div className="flex flex-col">
-              <div>{project.owner.name}</div>
-              <div className="text-xs text-gray-500">
-                {format(new Date(project.createdAt), "dd/MM/yyyy")}
-              </div>
-            </div>
-          </div>
-        </CardHeader>
-        <CardContent className="space-y-2">
-          {projectStatus?.value === "DONE" ? (
-            <Badge
-              variant="outline"
-              className="border-green-600 text-green-500">
-              {projectStatus?.label}
-            </Badge>
-          ) : projectStatus?.value === "TO_DO" ? (
-            <Badge variant="outline" className="border-blue-600 text-blue-500">
-              {projectStatus?.label}
-            </Badge>
-          ) : projectStatus?.value === "IN_PROGRESS" ? (
-            <Badge
-              variant="outline"
-              className="border-orange-600 text-orange-500">
-              {projectStatus?.label}
-            </Badge>
-          ) : projectStatus?.value === "CANCELED" ? (
-            <Badge variant="outline" className="border-red-600 text-red-500">
-              {projectStatus?.label}
-            </Badge>
-          ) : null}
-          <p className="text-sm text-gray-500">{project.description}</p>
-          <p className="text-sm text-gray-500">Task Completion Rate: 35/50</p>
-          <div className="w-full h-2 bg-gray-200 rounded-full">
-            <div
-              className="h-full text-xs text-center text-white bg-blue-500 rounded-full"
-              style={{
-                width: "70%",
-              }}
-            />
-            <p className="text-xs text-gray-500 text-right">70% Complete</p>
-          </div>
-        </CardContent>
-        <CardFooter className="flex justify-end">
-          <Link
-            href={`/projects/${project.id}`}
-            className="flex items-center cursor-pointer text-sm text-blue-800 hover:underline">
-            <span>View project</span>
-            <IoChevronForward className="w-3 h-3" />
-          </Link>
-        </CardFooter>
-      </Card> */}
       <Card>
         <CardHeader className="pb-4">
           <div className="flex items-center gap-2">
             <Image src={Logo} width={32} height={32} alt="Project logo" />
             <div>
               <CardTitle className="text-lg hover:underline cursor-pointer">
-                GO CLI Interface
+                {project.name}
               </CardTitle>
-              <Badge
-                variant="outline"
-                className="border-green-600 text-green-500">
-                Live
-              </Badge>
+              {projectStatus?.value === "LIVE" ? (
+                <Badge
+                  variant="outline"
+                  className="border-green-600 text-green-500">
+                  {projectStatus?.label}
+                </Badge>
+              ) : projectStatus?.value === "BUILDING" ? (
+                <Badge
+                  variant="outline"
+                  className="border-blue-600 text-blue-500">
+                  {projectStatus?.label}
+                </Badge>
+              ) : null}
             </div>
             <div className="ml-auto">
               <DropdownMenu>
