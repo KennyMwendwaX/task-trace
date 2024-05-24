8d4e9278f5d9f3e4a37d9c9be9f826454303758c
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-05-24 08:26:07 +0300

    feat: add et

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,14 +1,39 @@
+"use client";
+
 import TaskTable from "@/components/tables/UserTaskTable/TaskTable";
 import tasksData from "./tasks.json";
 import { TableColumns } from "@/components/tables/UserTaskTable/TableColumns";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { UserTask } from "@/lib/schema/TaskSchema";
+import { useQuery } from "@tanstack/react-query";
+import { auth } from "@/auth";
+import axios from "axios";
 
 export default function UserTasks() {
-  const tasks = tasksData?.map((task) => ({
-    ...task,
-    due_date: new Date(task.due_date),
-    createdAt: new Date(task.createdAt),
-  })) as UserTask[];
+  const {
+    data: tasksData,
+    isLoading: tasksIsLoading,
+    error: tasksError,
+  } = useQuery({
+    queryKey: ["user-tasks"],
+    queryFn: async () => {
+      const { data } = await axios.get(`/api/users/tasks`);
+      return data.tasks as UserTask[];
+    },
+  });
+  const tasks =
+    tasksData
+      ?.map((task) => ({
+        ...task,
+        due_date: new Date(task.due_date),
+        createdAt: new Date(task.createdAt),
+      }))
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime()) || [];
+
+  const tasksDone = tasks.filter((task) => task.status === "DONE");
+  const tasksTodo = tasks.filter((task) => task.status === "TO_DO");
+  const tasksInProgress = tasks.filter((task) => task.status === "IN_PROGRESS");
+  const tasksCanceled = tasks.filter((task) => task.status === "CANCELED");
   return (
     <>
       <div className="text-2xl font-bold tracking-tight">Tasks Page</div>
@@ -17,7 +42,40 @@ export default function UserTasks() {
         Here&apos;s a list of your tasks from your member projects!
       </div>
 
-      <TaskTable columns={TableColumns} data={tasks} />
+      <Tabs defaultValue="all" className="pt-2">
+        <TabsList className="grid grid-cols-5 w-[600px]">
+          <TabsTrigger value="all">All</TabsTrigger>
+          <TabsTrigger value="done">Done</TabsTrigger>
+          <TabsTrigger value="todo">Todo</TabsTrigger>
+          <TabsTrigger value="inprogress">In Progress</TabsTrigger>
+          <TabsTrigger value="canceled">Canceled</TabsTrigger>
+        </TabsList>
+        <TabsContent value="all">
+          <div className="pt-4">
+            <TaskTable data={tasks} columns={TableColumns} />
+          </div>
+        </TabsContent>
+        <TabsContent value="done">
+          <div className="pt-4">
+            <TaskTable data={tasksDone} columns={TableColumns} />
+          </div>
+        </TabsContent>
+        <TabsContent value="todo">
+          <div className="pt-4">
+            <TaskTable data={tasksTodo} columns={TableColumns} />
+          </div>
+        </TabsContent>
+        <TabsContent value="inprogress">
+          <div className="pt-4">
+            <TaskTable data={tasksInProgress} columns={TableColumns} />
+          </div>
+        </TabsContent>
+        <TabsContent value="canceled">
+          <div className="pt-4">
+            <TaskTable data={tasksCanceled} columns={TableColumns} />
+          </div>
+        </TabsContent>
+      </Tabs>
     </>
   );
 }
