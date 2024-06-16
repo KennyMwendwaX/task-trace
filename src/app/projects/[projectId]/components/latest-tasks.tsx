7be720c7e0765ad2557dfe8f89277a6003b0a899
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-06-16 10:19:31 +0300

    refactor: redesing the recent tasks card

--- src/app/projects/[projectId]/components/latest-tasks.tsx
+++ src/app/projects/[projectId]/components/latest-tasks.tsx
@@ -1,10 +1,28 @@
 "use client";
 
 import { ProjectTask } from "@/lib/schema/TaskSchema";
-import { Card } from "@/components/ui/card";
 import format from "date-fns/format";
 import { IoChevronForward } from "react-icons/io5";
 import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Button } from "@/components/ui/button";
+import { LuArrowUpRight } from "react-icons/lu";
+import { GoTasklist } from "react-icons/go";
+import { priorities } from "@/lib/config";
 
 type Props = {
   projectId: string;
@@ -23,63 +41,76 @@ export default function LatestTasks({ projectId, tasks }: Props) {
 
   return (
     <>
-      <Card className="w-[800px]">
-        <div className="flex justify-between p-2">
-          <div className="text-xl font-semibold leading-none tracking-tight">
-            Latest Tasks
+      <Card>
+        <CardHeader className="flex flex-row items-center">
+          <div className="grid gap-2">
+            <CardTitle className="text-xl">Recent Tasks</CardTitle>
+            <CardDescription>
+              Tasks recently added to the project.
+            </CardDescription>
           </div>
-          <Link
-            href={`/projects/${projectId}/tasks`}
-            className="flex items-center space-x-1 cursor-pointer text-blue-800 hover:underline">
-            <span>See all tasks</span>
-            <IoChevronForward className="w-5 h-5" />
-          </Link>
-        </div>
+          <Button asChild size="sm" className="ml-auto gap-1">
+            <Link href={`/projects/${projectId}/tasks`}>
+              View All
+              <LuArrowUpRight className="h-4 w-4" />
+            </Link>
+          </Button>
+        </CardHeader>
+        <CardContent>
+          {latestTasks.length > 0 ? (
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Task</TableHead>
+                  <TableHead>Due Date</TableHead>
+                  <TableHead>Priority</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {latestTasks.map((task) => {
+                  const priority = priorities.find(
+                    (priority) => priority.value === task.priority
+                  );
+                  const dueDate = format(task.due_date, "dd/MM/yyyy");
+                  if (!priority) return null;
+                  return (
+                    <TableRow key={task.id}>
+                      <TableCell className="font-medium hover:underline cursor-pointer">
+                        <Link
+                          className="hover:underline"
+                          href={`/projects/${projectId}/tasks/${task.id}`}>
+                          {task.name}
+                        </Link>
+                      </TableCell>
+                      <TableCell>{dueDate}</TableCell>
+                      <TableCell>
+                        <div className="flex items-center">
+                          {priority.value === "HIGH" ? (
+                            <priority.icon className="mr-1 h-5 w-5 text-muted-foreground text-red-600" />
+                          ) : priority.value === "MEDIUM" ? (
+                            <priority.icon className="mr-1 h-5 w-5 text-muted-foreground text-orange-500" />
+                          ) : priority.value === "LOW" ? (
+                            <priority.icon className="mr-1 h-5 w-5 text-muted-foreground text-blue-600" />
+                          ) : null}
+                          <span>{task.priority}</span>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          ) : (
+            <div className="mx-auto flex flex-col items-center justify-center text-center pt-16">
+              <GoTasklist className="h-12 w-12 text-muted-foreground" />
 
-        {latestTasks.length > 0 ? (
-          <div className="relative overflow-x-auto pt-2 pb-1">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Task
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Due Date
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Priority
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {latestTasks.map((task, index) => (
-                  <tr
-                    key={task.id}
-                    className={`bg-white ${
-                      index === latestTasks.length - 1 ? "" : "border-b"
-                    }`}>
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                      <Link
-                        className="hover:underline"
-                        href={`/projects/${projectId}/tasks/${task.id}`}>
-                        {task.name}
-                      </Link>
-                    </th>
-                    <td className="px-6 py-4">
-                      {format(task.due_date, "dd/MM/yyyy")}
-                    </td>
-                    <td className="px-6 py-4">{task.priority}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        ) : (
-          <div className="py-48 flex justify-center">No tasks available</div>
-        )}
+              <h3 className="mt-4 text-xl font-semibold">No tasks found</h3>
+              <p className="mb-4 mt-2 text-base text-muted-foreground">
+                You do not have any tasks.
+              </p>
+            </div>
+          )}
+        </CardContent>
       </Card>
     </>
   );
