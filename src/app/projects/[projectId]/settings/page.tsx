69a12bb1d0a69252746ea829cee6846d44d2d737
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-06-27 16:37:59 +0300

    feat: add project invitaton code card

--- src/app/projects/[projectId]/settings/page.tsx
+++ src/app/projects/[projectId]/settings/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { projectData } from "../components/project";
 import { Button } from "@/components/ui/button";
 import {
@@ -9,6 +11,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { LuClipboard } from "react-icons/lu";
 
 export default function Settings({
   params,
@@ -17,10 +20,48 @@ export default function Settings({
 }) {
   const projectId = params.projectId;
   const project = projectData;
+  const code = "ABCD1234FEDF";
+
+  const handleCopy = () => {
+    navigator.clipboard
+      .writeText(code)
+      .then(() => {
+        alert("Invitation code copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
+      });
+  };
 
   return (
     <>
       <main className="flex flex-1 flex-col p-4 lg:pt-4 lg:ml-[260px]">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle>Project Invite</CardTitle>
+            <CardDescription>
+              Share this code to invite new members to your project.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col items-center justify-center gap-3 pt-3">
+            <div className="rounded-md bg-primary px-6 py-3 text-3xl font-bold text-primary-foreground">
+              {code}
+            </div>
+
+            <p className="text-sm text-muted-foreground">
+              This code will expire in 7 days.
+            </p>
+          </CardContent>
+          <CardFooter className="border-t flex items-center justify-between py-2">
+            <Button className="flex items-center gap-1" onClick={handleCopy}>
+              <LuClipboard className="w-5 h-5" />
+              Copy
+            </Button>
+            <Button variant="outline" className="text-red-500">
+              Regenerate Code
+            </Button>
+          </CardFooter>
+        </Card>
         <Card>
           <CardHeader>
             <CardTitle>Update Project Name</CardTitle>
