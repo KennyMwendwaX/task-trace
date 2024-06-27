98231e0c059c93d8278252c4c0d0edb88740e30d
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-06-27 18:48:31 +0300

    feat: update drzzle config to match v0.21.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  dialect: "postgresql",
   schema: "./src/database/schema.ts",
   out: "./drizzle/migrations",
-  driver: "pg",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   verbose: true,
   strict: true,
