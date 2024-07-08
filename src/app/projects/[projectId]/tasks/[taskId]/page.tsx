3ca0103c65b37d05d2a43eb481291064cccd05b1
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-07-08 22:40:57 +0300

    refactor: format dateds with date-fns

--- src/app/projects/[projectId]/tasks/[taskId]/page.tsx
+++ src/app/projects/[projectId]/tasks/[taskId]/page.tsx
@@ -134,9 +134,9 @@ export default function TaskPage({ params }: TaskPageProps) {
   const status = statuses.find((s) => s.value === task.status);
   const priority = priorities.find((p) => p.value === task.priority);
 
-  const taskCreatedAt = format(task.createdAt, "dd MMM, yyyy • hh:mm:ss a..aa");
+  const taskCreatedAt = format(task.createdAt, "dd MMM, yyyy • hh:mm:ss a");
   const taskUpdatedAt = task.updatedAt
-    ? format(task.updatedAt, "dd MMM, yyyy • hh:mm:ss a..aa")
+    ? format(task.updatedAt, "dd MMM, yyyy • hh:mm:ss a")
     : null;
   const taskDueDate = format(task.dueDate, "dd MMM, yyyy");
 
