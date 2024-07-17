3f9ae1407a1acbaffccffa598fa550670c0b4035
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-07-17 23:06:45 +0300

    fix: padding on the tasks page

--- src/app/tasks/layout.tsx
+++ src/app/tasks/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="container mx-auto px-6 py-4 bg-muted/40 min-h-screen md:px-10 lg:px-14">
+    <main className="container mx-auto px-4 py-4 bg-muted/40 min-h-screen md:px-10 lg:px-14">
       {children}
     </main>
   );
