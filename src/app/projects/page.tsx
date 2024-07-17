f11fa595daefefbbbdddc0a80b19b0392dc96082
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-07-17 23:07:44 +0300

    fix: padding on the projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -78,7 +78,7 @@ export default function Projects() {
     filteredUserProjects.length > 0 || filteredMemberProjects.length > 0;
 
   return (
-    <main className="container mx-auto px-8 py-4 bg-muted/40 min-h-screen md:px-10 lg:px-14">
+    <main className="container mx-auto px-4 py-4 bg-muted/40 min-h-screen md:px-10 lg:px-14">
       {hasProjects || search ? (
         <>
           <div className="flex items-center justify-between sm:gap-2">
