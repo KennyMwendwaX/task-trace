2a80b11e22b106e0da9ae6674d9fde9847821eaf
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-07-22 22:16:11 +0300

    feat: mout MemberLeaderboard to analytics page

--- src/app/projects/[projectId]/analytics/page.tsx
+++ src/app/projects/[projectId]/analytics/page.tsx
@@ -2,6 +2,7 @@
 
 import { ProjectTask } from "@/lib/schema/TaskSchema";
 import { tasksData } from "../components/tasks";
+import MemberLeaderboard from "./components/member-leaderboard";
 import TaskStatusChart from "./components/task-status-chart";
 
 export default function Analytics() {
@@ -16,6 +17,7 @@ export default function Analytics() {
     <main className="flex flex-1 flex-col p-4 lg:pt-4 lg:ml-[260px]">
       <div className="text-2xl font-bold tracking-tight">Analytics</div>
       <div className="w-full grid grid-cols-1 gap-4 mt-6 md:grid-cols-2">
+        <MemberLeaderboard tasks={tasks} />
         <TaskStatusChart tasks={tasks} />
       </div>
     </main>
