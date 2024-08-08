4600ec4b837290b3460e0a5ec65cd0ecfae6bced
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-08-08 17:28:01 +0300

    feat: add no project and no task state on the task edit page

--- src/app/projects/[projectId]/tasks/[taskId]/edit/page.tsx
+++ src/app/projects/[projectId]/tasks/[taskId]/edit/page.tsx
@@ -141,6 +141,68 @@ export default function EditTaskPage({ params }: EditTaskPageProps) {
     },
   });
 
+  if (!project) {
+    return (
+      <main className="flex flex-1 flex-col gap-2 p-4 lg:pt-4 lg:ml-[260px]">
+        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm min-h-[560px]">
+          <div className="flex flex-col items-center gap-1 text-center">
+            <div className="bg-gray-100 rounded-full p-4 inline-block mb-4">
+              <MdOutlineFolderOff className="h-12 w-12 text-gray-400" />
+            </div>
+            <h2 className="text-3xl font-bold text-gray-800 mb-3">
+              No Project Found
+            </h2>
+            <p className="text-gray-600 mb-4">
+              The project you&apos;re looking for doesn&apos;t exist or has been
+              removed.
+            </p>
+            <div className="flex justify-center">
+              <Button
+                size="lg"
+                variant="default"
+                className="flex items-center justify-center gap-2 rounded-full"
+                onClick={() => router.push("/dashboard")}>
+                <LuChevronLeft className="w-5 h-5" />
+                Return to Dashboard
+              </Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
+  if (!task) {
+    return (
+      <main className="flex flex-1 flex-col gap-2 p-4 lg:pt-4 lg:ml-[260px]">
+        <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm min-h-[560px]">
+          <div className="flex flex-col items-center gap-1 text-center">
+            <div className="bg-gray-100 rounded-full p-4 inline-block mb-4">
+              <BsListTask className="h-12 w-12 text-gray-400" />
+            </div>
+            <h2 className="text-3xl font-bold text-gray-800 mb-3">
+              No Task Found
+            </h2>
+            <p className="text-gray-600 mb-4">
+              The task you&apos;re looking for doesn&apos;t exist or has been
+              removed.
+            </p>
+            <div className="flex justify-center">
+              <Button
+                size="lg"
+                variant="default"
+                className="flex items-center justify-center gap-2 rounded-full"
+                onClick={() => router.push(`/projects/${projectId}/tasks`)}>
+                <LuChevronLeft className="w-5 h-5" />
+                Go Back to Tasks
+              </Button>
+            </div>
+          </div>
+        </div>
+      </main>
+    );
+  }
+
   async function onSubmit(values: TaskFormValues) {
     updateTask(values);
   }
