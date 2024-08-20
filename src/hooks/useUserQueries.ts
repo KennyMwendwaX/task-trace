0bbc7559802c47ee6472a73a5464ee9f53ea3245
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-08-20 23:27:47 +0300

    feat: add useUserSignupMutation

--- src/hooks/useUserQueries.ts
+++ src/hooks/useUserQueries.ts
@@ -1,12 +1,26 @@
-import { User } from "@/lib/schema/UserSchema";
+import { SignupValues, User } from "@/lib/schema/UserSchema";
 import { useUserStore } from "./useUserStore";
-import { useQuery, UseQueryResult } from "@tanstack/react-query";
+import {
+  useMutation,
+  UseMutationResult,
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from "@tanstack/react-query";
 import { fetchUsers } from "@/lib/api/users";
 import { useEffect } from "react";
 import { fetchUserProjects } from "@/lib/api/projects";
 import { ExtendedProject } from "@/lib/schema/ProjectSchema";
 import { UserTask } from "@/lib/schema/TaskSchema";
 import { fetchUserTasks } from "@/lib/api/tasks";
+
+interface SignupResponse {
+  message: string;
+}
+
+interface SignupError {
+  message: string;
+}
 
 export const useUsersQuery = (): UseQueryResult<User[], Error> => {
   const setUsers = useUserStore((state) => state.setUsers);
@@ -60,4 +74,35 @@ export const useUsersTasksQuery = (
   }, [result.data, setUserTasks]);
 
   return result;
+};
+
+export const useUserSignupMutation = (): UseMutationResult<
+  SignupResponse,
+  SignupError,
+  SignupValues,
+  unknown
+> => {
+  return useMutation<SignupResponse, SignupError, SignupValues, unknown>({
+    mutationFn: async (values: SignupValues) => {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: values.name,
+          email: values.email,
+          password: values.password,
+        }),
+      };
+      const response = await fetch("/api/auth/register", options);
+
+      if (!response.ok) {
+        const error: SignupError = await response.json();
+        throw error;
+      }
+
+      return response.json();
+    },
+  });
 };
