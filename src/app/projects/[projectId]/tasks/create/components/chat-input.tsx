ea63d986246b064c7acba29fdc22443a5e108e77
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-08-26 23:15:49 +0300

    feat: toggle formatting on editor

--- src/app/projects/[projectId]/tasks/create/components/chat-input.tsx
+++ src/app/projects/[projectId]/tasks/create/components/chat-input.tsx
@@ -1,13 +1,19 @@
 import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef } from "react";
 
 const Editor = dynamic(() => import("./editor"), { ssr: false });
 
 export default function ChatInput() {
+  const editorRef = useRef<Quill | null>(null);
   return (
-    <>
-      <div className="w-full">
-        <Editor />
-      </div>
-    </>
+    <div className="w-full">
+      <Editor
+        variant="create"
+        placeholder="Task description"
+        disabled={false}
+        innerRef={editorRef}
+      />
+    </div>
   );
 }
--- src/app/projects/[projectId]/tasks/create/components/editor.tsx
+++ src/app/projects/[projectId]/tasks/create/components/editor.tsx
@@ -1,13 +1,53 @@
-import { Button } from "@/components/ui/button";
 import Quill, { QuillOptions } from "quill";
+import { Delta, Op } from "quill/core";
 import "quill/dist/quill.snow.css";
-import { useEffect, useRef } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  MutableRefObject,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from "react";
 import { LuImage, LuSmile } from "react-icons/lu";
 import { MdSend } from "react-icons/md";
 import { PiTextAa } from "react-icons/pi";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
-export default function Editor() {
+interface EditorProps {
+  variant?: "create" | "edit";
+  placeholder?: string;
+  defaultValue?: Delta | Op[];
+  disabled?: boolean;
+  innerRef?: MutableRefObject<Quill | null>;
+}
+
+export default function Editor({
+  variant,
+  placeholder,
+  defaultValue = [],
+  disabled = false,
+  innerRef,
+}: EditorProps) {
+  const [text, setText] = useState("");
+  const [isToolbarVisible, setIsToolbarVisible] = useState(true);
+
   const containerRef = useRef<HTMLDivElement>(null);
+  const placeholderRef = useRef(placeholder);
+  const quillRef = useRef<Quill | null>(null);
+  const defaultValueRef = useRef(defaultValue);
+  const disabledRef = useRef(disabled);
+
+  useLayoutEffect(() => {
+    placeholderRef.current = placeholder;
+    defaultValueRef.current = defaultValue;
+    disabledRef.current = disabled;
+  });
 
   useEffect(() => {
     if (!containerRef.current) return;
@@ -19,49 +59,112 @@ export default function Editor() {
 
     const options: QuillOptions = {
       theme: "snow",
+      placeholder: placeholderRef.current,
     };
 
     const quill = new Quill(editorContainer, options);
+    quillRef.current = quill;
 
+    if (innerRef) {
+      innerRef.current = quill;
+    }
+
+    quill.setContents(defaultValueRef.current);
+    setText(quill.getText());
+
+    quill.on(Quill.events.TEXT_CHANGE, () => {
+      setText(quill.getText());
+    });
     return () => {
+      quill.off(Quill.events.TEXT_CHANGE);
       if (container) {
         container.innerHTML = "";
       }
+      if (quillRef.current) {
+        quillRef.current = null;
+      }
+      if (innerRef) {
+        innerRef.current = null;
+      }
     };
-  }, []);
+  }, [innerRef]);
+
+  const toggleToolbar = () => {
+    setIsToolbarVisible((current) => !current);
+    const toolbarElement = containerRef.current?.querySelector(".ql-toolbar");
+
+    if (toolbarElement) {
+      toolbarElement.classList.toggle("hidden");
+    }
+  };
+
+  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  console.log({ isEmpty, text });
   return (
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
-        <div ref={containerRef} className="h-full ql-custom" />
+        <div ref={containerRef} className="h-48 ql-custom" />
         <div className="flex px-2 pb-2 z-[5]">
-          <Button
-            disabled={false}
-            size="icon"
-            variant="ghost"
-            onClick={() => {}}>
-            <PiTextAa className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            size="icon"
-            variant="ghost"
-            onClick={() => {}}>
-            <LuSmile className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            size="icon"
-            variant="ghost"
-            onClick={() => {}}>
-            <LuImage className="size-4" />
-          </Button>
-          <Button
-            disabled={false}
-            onClick={() => {}}
-            className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
-            size="icon">
-            <MdSend className="size-4" />
-          </Button>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  size="icon"
+                  variant={isToolbarVisible ? "default" : "ghost"}
+                  onClick={() => toggleToolbar()}>
+                  <PiTextAa className="size-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                {isToolbarVisible ? "Hide formatting" : "Show formatting"}
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  disabled={false}
+                  size="icon"
+                  variant="ghost"
+                  onClick={() => {}}>
+                  <LuSmile className="size-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Insert emoji</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  disabled={false}
+                  size="icon"
+                  variant="ghost"
+                  onClick={() => {}}>
+                  <LuImage className="size-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Insert image</p>
+              </TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  disabled={false}
+                  onClick={() => {}}
+                  className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white"
+                  size="icon">
+                  <MdSend className="size-4" />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Send message</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
         </div>
       </div>
     </div>
