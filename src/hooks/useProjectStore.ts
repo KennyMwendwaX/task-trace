780b5d01b4cc69c00b8bca4dfec9cba818a38e4a
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-09-04 17:45:51 +0300

    feat: add invitation code to project store

--- src/hooks/useProjectStore.ts
+++ src/hooks/useProjectStore.ts
@@ -9,11 +9,13 @@ interface ProjectStore {
   members: Member[];
   task: ProjectTask | null;
   tasks: ProjectTask[];
+  invitationCode: string | null;
   setProject: (project: Project) => void;
   setProjects: (projects: Project[]) => void;
   setMembers: (members: Member[]) => void;
   setProjectTask: (task: ProjectTask) => void;
   setProjectTasks: (tasks: ProjectTask[]) => void;
+  setInvitationCode: (invitationCode: string) => void;
 }
 
 export const useProjectStore = create<ProjectStore>((set) => ({
@@ -22,9 +24,11 @@ export const useProjectStore = create<ProjectStore>((set) => ({
   members: [],
   task: null,
   tasks: [],
+  invitationCode: null,
   setProject: (project) => set({ project }),
   setProjects: (projects) => set({ projects }),
   setMembers: (members) => set({ members }),
   setProjectTask: (task) => set({ task }),
   setProjectTasks: (tasks) => set({ tasks }),
+  setInvitationCode: (invitationCode) => set({ invitationCode }),
 }));
