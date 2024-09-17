37a641c4d8035d9d81998d3f4ef73fac4658d93c
kenny-mwendwa <kennymwendwadev@gmail.com>
2024-09-17 19:59:32 +0300

    refactor: remove unnecesary comments

--- src/auth.ts
+++ src/auth.ts
@@ -1,16 +1,7 @@
-import NextAuth, { DefaultSession } from "next-auth";
+import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import db from "@/database/db";
-
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id: string;
-//       role: "ADMIN" | "USER";
-//     } & DefaultSession["user"];
-//   }
-// }
 
 export const {
   handlers: { GET, POST },
@@ -18,35 +9,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  // callbacks: {
-  //   async session({ token, session }) {
-  //     if (token.sub && session.user) {
-  //       session.user.id = token.sub;
-  //     }
-
-  //     if (token.role && session.user) {
-  //       session.user.role = token.role as "ADMIN" | "USER";
-  //     }
-
-  //     return session;
-  //   },
-  //   async jwt({ token }) {
-  //     if (!token.sub) return token;
-
-  //     if (token.sub == undefined) return token;
-
-  //     const existingUser = await db.query.users.findFirst({
-  //       where: (existingUser, { eq }) => eq(existingUser.id, token.sub!),
-  //     });
-
-  //     if (!existingUser) return token;
-
-  //     token.role = existingUser.role;
-  //     return token;
-  //   },
-  // },
   adapter: DrizzleAdapter(db),
-  secret: process.env.NEXTAUTH_SECRET,
   session: { strategy: "jwt" },
   ...authConfig,
 });
