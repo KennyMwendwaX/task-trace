e0c5e43fdc0e9c85ea633be8656b670f63a2076d
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-10-04 10:37:28 +0300

    feat: add session providr to the main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,13 +1,16 @@
+import { auth } from "@/auth";
 import Layout from "@/components/layout";
+import SessionProvider from "@/providers/SessionProvider";
 
 export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
   return (
-    <>
+    <SessionProvider session={session}>
       <Layout>{children}</Layout>
-    </>
+    </SessionProvider>
   );
 }
--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -1,18 +1,4 @@
 "use client";
 
-import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
-
-export default function SessionProvider({
-  children,
-  session,
-}: {
-  children: React.ReactNode;
-  session: Session | null;
-}) {
-  return (
-    <NextAuthSessionProvider session={session}>
-      {children}
-    </NextAuthSessionProvider>
-  );
-}
+import { SessionProvider } from "next-auth/react";
+export default SessionProvider;
