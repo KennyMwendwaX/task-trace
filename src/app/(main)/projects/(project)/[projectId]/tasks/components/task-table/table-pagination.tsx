c5cd9d8b529c330b9d25526bbc22b1caf3a465f8
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-10-12 16:38:39 +0300

    fix: task table pagination component on sm screens

--- src/app/(main)/projects/(project)/[projectId]/tasks/components/task-table/table-pagination.tsx
+++ src/app/(main)/projects/(project)/[projectId]/tasks/components/task-table/table-pagination.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Table } from "@tanstack/react-table";
 import {
   ChevronLeftIcon,
@@ -22,12 +23,12 @@ export default function TablePagination<TData>({
   table,
 }: TablePaginationProps<TData>) {
   return (
-    <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:justify-between sm:space-y-0 px-2">
+    <div className="flex flex-col space-y-4 px-2 py-4 sm:flex-row sm:items-center sm:justify-between sm:space-y-0">
       <div className="text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:space-x-6 sm:space-y-0 lg:space-x-8">
+      <div className="flex flex-col space-y-4 sm:flex-row sm:items-center sm:space-x-4 sm:space-y-0">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows</p>
           <Select
@@ -47,14 +48,10 @@ export default function TablePagination<TData>({
             </SelectContent>
           </Select>
         </div>
-        <div className="flex items-center justify-center text-sm font-medium">
-          Page {table.getState().pagination.pageIndex + 1} of{" "}
-          {table.getPageCount()}
-        </div>
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center justify-between space-x-2 sm:justify-end">
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 sm:flex"
+            className="h-8 w-8 p-0"
             onClick={() => table.setPageIndex(0)}
             disabled={!table.getCanPreviousPage()}>
             <span className="sr-only">Go to first page</span>
@@ -68,6 +65,10 @@ export default function TablePagination<TData>({
             <span className="sr-only">Go to previous page</span>
             <ChevronLeftIcon className="h-4 w-4" />
           </Button>
+          <div className="text-sm font-medium">
+            Page {table.getState().pagination.pageIndex + 1} of{" "}
+            {table.getPageCount()}
+          </div>
           <Button
             variant="outline"
             className="h-8 w-8 p-0"
@@ -78,7 +79,7 @@ export default function TablePagination<TData>({
           </Button>
           <Button
             variant="outline"
-            className="hidden h-8 w-8 p-0 sm:flex"
+            className="h-8 w-8 p-0"
             onClick={() => table.setPageIndex(table.getPageCount() - 1)}
             disabled={!table.getCanNextPage()}>
             <span className="sr-only">Go to last page</span>
