2cd9c7698c0eed3b122356a70b0bc583ee9a4a98
KennyMwendwaX <kennymwendwadev@gmail.com>
2024-11-12 11:30:36 +0300

    feat: remove users from the user store

--- src/hooks/useUserStore.ts
+++ src/hooks/useUserStore.ts
@@ -1,26 +1,21 @@
 import { UserMembershipRequest } from "@/lib/schema/MembershipRequests";
 import { MemberProject } from "@/lib/schema/ProjectSchema";
 import { UserTask } from "@/lib/schema/TaskSchema";
-import { User } from "@/lib/schema/UserSchema";
 import { create } from "zustand";
 
 interface UserStore {
-  users: User[];
   projects: MemberProject[];
   tasks: UserTask[];
   requests: UserMembershipRequest[];
-  setUsers: (users: User[]) => void;
   setUserProjects: (projects: MemberProject[]) => void;
   setUserTasks: (tasks: UserTask[]) => void;
   setUserUserMembershipRequest: (requests: UserMembershipRequest[]) => void;
 }
 
 export const useUserStore = create<UserStore>((set) => ({
-  users: [],
   projects: [],
   tasks: [],
   requests: [],
-  setUsers: (users) => set({ users }),
   setUserProjects: (projects) => set({ projects }),
   setUserTasks: (tasks) => set({ tasks }),
   setUserUserMembershipRequest: (requests) => set({ requests }),
